256601c75b83a12409acfb3ed5e0d640a95b175d
salina225 <70594148+salina225@users.noreply.github.com>
2021-04-20 18:09:40 -0500

    Container.js

---
+++ src/Container.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Container = ({ children }) => {
+  return (
+    <div
+    style={containerStyle}
+    >
+      {children}
+    </div>
+  );
+};
+
+const containerStyle = {
+  width: 900
+  , margin: '0 auto'
+  , padding: '20px 0px'
+};
+
+export default Container; 
